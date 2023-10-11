b5678510da1cb8fa690fc81b6c149b4c395a0fab
Karl Poulson <programmingnpc@gmail.com>
2023-10-11 16:09:01 -0600

    First commit

---
+++ cnnModel.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np
+
+
+"""
+This class will be used to import the cleansed and transformed data
+and create a CNN model to train the data
+"""
---
+++ dataCleansing.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import numpy as np
+import keras
+
+
+"""
+The files is used to explore, clean and transform the data
+Features will also include creation of new features 
+Image flipping, rotation, zooming, etc. will be done using Keras
+"""
